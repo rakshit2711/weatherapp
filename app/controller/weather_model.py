35e72bfa4aa1b59c17a88ec32cb56170c4f1e743
rakshit2711 <rakshitmalik2010@gmail.com>
2024-05-06 12:26:22 +0530

    gitignore

--- app/controller/weather_model.py
+++ app/controller/weather_model.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from 
 
 weather_router =APIRouter()
 
 @weather_router.get("/weather")
 def get_weather_by_city(city:str):
-    
+    weathe
     return {"city":city}
